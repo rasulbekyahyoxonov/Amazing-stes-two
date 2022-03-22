2442be21054c8087f23c0af27231fca6ef203f96
rasulbekyahyoxonov <rasulbekyahyoxonov2gmail.com>
2022-03-22 15:29:16 +0500

    Nima

---
+++ JavaScript.js
@@ -0,0 +1,35 @@
+
+//  Scroolbar ********
+
+let progress = document.getElementById('progressbar');
+let totalHeight = document.body.scrollHeight - 
+    window.innerHeight;
+window.onscroll = function() {
+    let progressHeight = (window.pageYOffset / 
+    totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+}
+
+// Counter number 
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+    counter.innerText = '0';
+    
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const c = +counter.innerText;
+
+        const increment = target / 250;
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 20);
+        }
+        else {
+            counter.innerText = target;
+        }
+    };
+    updateCounter();
+});
