8919d097f35ded73412cf7eb9e9911e7ad522a4a
rasulbekyahyoxonov <rasulbekyahyoxonov2gmail.com>
2022-03-27 13:37:22 +0500

    sdsdsd

--- java/JavaScript.js
+++ java/JavaScript.js
@@ -19,7 +19,7 @@ counters.forEach((counter) => {
     
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target');
-        const c = +counter.innerText;
+        const c = + counter.innerText;
 
         const increment = target / 250;
 
@@ -33,3 +33,25 @@ counters.forEach((counter) => {
     };
     updateCounter();
 });
+
+// Scroll Reveal
+
+ScrollReveal ({
+    reset: false,
+    distance: '80px',
+    duration: 1000
+});
+
+ScrollReveal().reveal('.explore',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.carddiv1',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.carddiv2',{delay: 300, origin: 'bottom'});
+ScrollReveal().reveal('.carddiv3',{delay: 400, origin: 'bottom'});
+ScrollReveal().reveal('.carddiv4',{delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.top-destination',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.bottom-destination',{delay: 600, origin: 'bottom'});
+ScrollReveal().reveal('#bottom-packages',{delay: 600, origin: 'bottom'});
+ScrollReveal().reveal('.counter-div',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.flex-corusel',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.articles-flex',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.newsletter-div',{delay: 200, origin: 'bottom'});
+ScrollReveal().reveal('.flex-footer',{delay: 200, origin: 'bottom'});
